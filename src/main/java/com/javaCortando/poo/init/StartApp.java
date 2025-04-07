8bfb9e4911b1aa1b9386fb992ebf3ffbc2754531
giulms <expertgiulliano@gmail.com>
2025-04-07 02:56:54 -0300

    Criar um barbeiro ao inicializar o programa

--- src/main/java/com/javaCortando/poo/init/StartApp.java
+++ src/main/java/com/javaCortando/poo/init/StartApp.java
@@ -1,8 +1,33 @@
 package com.javaCortando.poo.init;
 
-import org.springframework.stereotype.Component;
+import com.javaCortando.poo.model.Barbeiro;
+import com.javaCortando.poo.repository.RepositoryBarbeiro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Component
-public class StartApp {
+@SpringBootApplication
+public class StartApp implements CommandLineRunner {
 
+    @Autowired
+    private RepositoryBarbeiro barbeiroRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        Barbeiro barbeiroInicial = new Barbeiro();
+        barbeiroInicial.setNome("João");
+        barbeiroInicial.setTelefone("11999999999");
+        barbeiroInicial.setEmail("joao@barbearia.com");
+        barbeiroInicial.setSenha("senha123");
+        barbeiroInicial.setHorarioInicial(8.0f);
+        barbeiroInicial.setHorarioFinal(18.0f);
+        barbeiroInicial.setTempoPorCorte(0.5f);
+        barbeiroRepository.save(barbeiroInicial);
+        System.out.println("Barbeiro Salvo com sucesso!");
+    }
 }
