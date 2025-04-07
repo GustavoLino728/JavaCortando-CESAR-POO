c909a1ded92c71986df64a6e25b964b52937b521
giulms <expertgiulliano@gmail.com>
2025-04-07 03:17:52 -0300

    Correção da criação do barbeiro

--- src/main/java/com/javaCortando/poo/init/StartApp.java
+++ src/main/java/com/javaCortando/poo/init/StartApp.java
@@ -1,7 +1,6 @@
 package com.javaCortando.poo.init;
 
-import com.javaCortando.poo.model.Barbeiro;
-import com.javaCortando.poo.repository.RepositoryBarbeiro;
+import com.javaCortando.poo.service.ServiceBarbeiro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartApp implements CommandLineRunner {
 
     @Autowired
-    private RepositoryBarbeiro barbeiroRepository;
+    private ServiceBarbeiro serviceBarbeiro;
 
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
@@ -19,15 +18,6 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Barbeiro barbeiroInicial = new Barbeiro();
-        barbeiroInicial.setNome("João");
-        barbeiroInicial.setTelefone("11999999999");
-        barbeiroInicial.setEmail("joao@barbearia.com");
-        barbeiroInicial.setSenha("senha123");
-        barbeiroInicial.setHorarioInicial(8.0f);
-        barbeiroInicial.setHorarioFinal(18.0f);
-        barbeiroInicial.setTempoPorCorte(0.5f);
-        barbeiroRepository.save(barbeiroInicial);
-        System.out.println("Barbeiro Salvo com sucesso!");
+        serviceBarbeiro.salvarBarbeiroInicial();
     }
 }
--- src/main/java/com/javaCortando/poo/service/ServiceBarbeiro.java
+++ src/main/java/com/javaCortando/poo/service/ServiceBarbeiro.java
@@ -12,8 +12,21 @@ public class ServiceBarbeiro {
     @Autowired
     private RepositoryBarbeiro repositoryBarbeiro;
 
-    public void salvarBarbeiro(Barbeiro barbeiro){
-        repositoryBarbeiro.save(barbeiro);
+    public void salvarBarbeiroInicial(){
+        Barbeiro barbeiroInicial = new Barbeiro();
+        barbeiroInicial.setNome("João");
+        barbeiroInicial.setTelefone("81940028922");
+        barbeiroInicial.setEmail("joao@barbearia.com");
+        barbeiroInicial.setSenha("senha123");
+        barbeiroInicial.setHorarioInicial(8.0f);
+        barbeiroInicial.setHorarioFinal(18.0f);
+        barbeiroInicial.setTempoPorCorte(0.5f);
+        repositoryBarbeiro.save(barbeiroInicial);
+        System.out.println("Barbeiro Salvo com sucesso!");
+    }
+
+    public void definirHorarioFuncionamento(Barbeiro barbeiro){
+
     }
 
 }
